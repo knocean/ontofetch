361935c687a63596611661267270406fbd985f3f
Rebecca Tauber <becky.tauber@ninepts.com>
2017-10-06 08:18:10 -0400

    Handle redirects more efficiently and add details to metadata map (#1)

--- src/ontofetch/core.clj
+++ src/ontofetch/core.clj
@@ -1,41 +1,30 @@
 (ns ontofetch.core
+  (:require
+    [clojure.xml :as xml]
+    [clojure.java.io :as io]
+    [clojure.zip :as zip]
+    [org.httpkit.client :as http])
   (:gen-class))
-(require '[clojure.xml :as xml]
-         '[clojure.java.io :as io]
-         '[clojure.zip :as zip])
 
 ;; TODO: General error handling
-;;       What if URL does not return any content, or incorrect content?
-;;       nil IRIs, etc.
-
-;; TODO: Issues with PR & CHEBI bc they redirect to OBO files
 
 (defn validate-dir
   "Checks if a directory is in proper format and that it does not exist. 
    If so, returns dir name as str."
   [dir]
-	;; TODO: not finding escaped backslash
-  (if-not (or (re-find #"[^\w\\s]" dir) (.isDirectory (io/file dir)))
-    (identity dir)
+  (when-not (re-matches #"[A-Za-z0-9_]+" dir)
     (throw (Exception. "Directory name can only include letters, numbers, or 
-			underscores, and must not already exist in the file system."))))
-
-(defn return-content
-  "Checks if the url content is XML. If not, finds a URL to redir to
-   (assuming 'this page has moved' & provides new URL). If XML, returns content."
-  [url]
-  (if-not (re-find #"xml version" (slurp url))
-    (recur (first (re-find
-    	           #"(http|ftp|https)://([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?"
-                   (slurp url))))
-    (slurp url)))
+      underscores.")))
+  (when (.isDirectory (io/file dir))
+    (throw (Exception. "Directory must not already exist in the file system.")))
+  dir)
 
 (defn fetch!
   "Makes a directory and downloads given ontology to it. Returns path to file."
-  [dir url]
-  (do (let [filepath (str dir "/" (last (clojure.string/split url #"/")))]
+  [dir final-url]
+  (do (let [filepath (str dir "/" (last (clojure.string/split final-url #"/")))]
         (.mkdir (java.io.File. (validate-dir dir)))
-        (let [content (return-content url)]
+        (let [content (slurp final-url)]
           (spit filepath content))
         filepath)))
 
@@ -44,8 +33,9 @@
   [filepath]
   (zip/xml-zip (xml/parse (io/file filepath))))
 
-;; TODO: What if you don't have an ontology def in the file?
-;;		 Implement validation checks on IRIs.
+;; TODO: change get-iri methods to find IRIs by name of element
+;;       instead of assuming their location.
+
 (defn get-ontology-iri
   "Returns the ontology IRI from a RDF/XML OWL file."
   [xml-tree]
@@ -61,6 +51,22 @@
               zip/down
               zip/node) [:attrs :rdf:resource]))
 
+;; TODO: Look for imports (will not be in the same place every time)
+;;       use the name of the element instead of just the position
+
+(defn get-redirects
+  "Manually follows redirects and returns a vector containing all redirect URLs.
+   The final URL with content is the last entry."
+  [url]
+  (loop [redirs []
+         new-url url]
+    (let [{:keys [status headers body error] :as res} @(http/request {:url new-url :method :head :follow-redirects false})]
+      (case status
+        200 (conj redirs new-url)
+        (301 302 303 307 308) (recur (conj redirs new-url) (:location headers))
+        304 nil
+        (throw (Exception. "Unhandled status."))))))
+
 (defn map-ontology
   "Returns a map of the ontology metadata."
   [filepath]
@@ -68,7 +74,24 @@
   	{:ontology-iri (get-ontology-iri xml-tree),
      :version-iri (get-version-iri xml-tree)}))
 
+(defn map-request
+  "Returns a map of the request details for a given ontology (which acts as the key)."
+  [filepath redirs metadata]
+  {(-> filepath
+       (clojure.string/split #"/")
+       last
+       (clojure.string/split #"\.")
+       first) {:request-url (first redirs),
+               :location filepath,
+               :redirect-path redirs,
+               :metadata metadata}})
+
+;; TODO: store metadata in catalog.edn file
+;;       including list of redirects & IRIs
+
 (defn -main
-  "Just downloads the ontology into given dir and returns IRIs."
+  "Just downloads the ontology into given dir and returns metadata."
   [& args]
-  (map-ontology (fetch! (first args) (second args))))
+  (let [redirs (get-redirects (second args))]
+    (let [filepath (fetch! (first args) (last redirs))]
+      (map-request filepath redirs (map-ontology filepath)))))
