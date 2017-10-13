c66280cd9e95217b65821c05a4efebaeaf295815
Rebecca Tauber <becky.tauber@ninepts.com>
2017-10-13 12:07:38 -0400

    Add get-imports & change zipper method

--- src/ontofetch/core.clj
+++ src/ontofetch/core.clj
@@ -11,6 +11,7 @@
 
 ;; TODO: General error handling (error logs)
 
+;; TODO: Deal with if catalog doesn't exist
 (def +catalog+ "catalog.edn")
 (def date (.format (java.text.SimpleDateFormat. "yyyy-MM-dd") (java.util.Date.)))
 (def ont-metadata (atom (edn/read-string (slurp +catalog+))))
@@ -48,37 +49,50 @@
         304 nil
         (throw (Exception. "Unhandled status."))))))
 
-(defn zip-xml
-  "Parses a an XML file and creates a zipper to traverse."
+;; TODO: It's still parsing the whole file in (takes ~10 seconds for GO)
+(defn get-ont-metadata
+  "Returns the XML node containing the ontology metadata."
   [filepath]
-  (zip/xml-zip (xml/parse (io/file filepath))))
+  ((fn find-md [xml]
+     (if-not (zip/end? xml)
+       (if (= (:tag (zip/node xml)) :owl:Ontology)
+         (zip/node xml)
+         (find-md (zip/next xml)))))
+   (zip/xml-zip (xml/parse (io/file filepath)))))
 
 (defn get-ontology-iri
   "Returns the ontology IRI from an RDF/XML OWL file."
-  [xml-tree]
-  (if-not (zip/end? xml-tree)
-    (let [details (zip/node xml-tree)]
-      (if (= (:tag details) :owl:Ontology)
-        (get-in details [:attrs :rdf:about])
-        (recur (zip/next xml-tree))))))
+  [xml]
+  (get-in xml [:attrs :rdf:about]))
 
 (defn get-version-iri
-  "Returns the version IRI from an RDF/XML OWL file"
-  [xml-tree]
-  (if-not (zip/end? xml-tree)
-    (let [details (zip/node xml-tree)]
-      (if (= (:tag details) :owl:versionIRI)
-        (get-in details [:attrs :rdf:resource])
-        (recur (zip/next xml-tree))))))
+  "Returns the version IRI from an RDF/XML OWL file."
+  [xml]
+  (loop [n 0]
+    (if (< n (count xml))
+      (let [content (nth (:content xml) n)]
+        (if (= (:tag content) :owl:versionIRI)
+          (get-in content [:attrs :rdf:resource])
+          (recur (+ n 1)))))))
 
-;; TODO: List of imports
+(defn get-imports
+  "Gets a list of import URLs from an RDF/XML OWL file."
+  [xml]
+  (loop [n 0 imports []]
+    (if (< n (count xml))
+      (let [content (nth (:content xml) n)]
+        (if (= (:tag content) :owl:imports)
+          (recur (+ n 1) (conj imports (get-in content [:attrs :rdf:resource])))
+          (recur (+ n 1) imports)))
+      imports)))
 
 (defn map-ontology
   "Returns a map of the ontology metadata."
   [filepath]
-  (let [xml-tree (zip-xml filepath)]
-    {:ontology-iri (get-ontology-iri xml-tree),
-     :version-iri (get-version-iri xml-tree)}))
+  (let [xml (get-ont-metadata filepath)]
+    {:ontology-iri (get-ontology-iri xml),
+     :version-iri (get-version-iri xml)
+     :imports (get-imports xml)}))
 
 (defn get-ontology-key
   "Returns a keyword based on the filename to be used as key in catalog.edn."
@@ -93,12 +107,13 @@
 (defn map-request
   "Returns a map of the request details for a given ontology (which acts as the key)."
   [filepath redirs metadata]
-  {(get-ontology-key filepath) {:request-url (first redirs),
-                                :directory (first (s/split filepath #"/")),
-                                :request-date date,
-                                :location filepath,
-                                :redirect-path redirs,
-                                :metadata metadata}})
+  {(get-ontology-key filepath)
+   {:request-url (first redirs),
+    :directory (first (s/split filepath #"/")),
+    :request-date date,
+    :location filepath,
+    :redirect-path redirs,
+    :metadata metadata}})
 
 (defn create-artifact-record
   "Creates a record of the last request made for the requested ontology."
@@ -107,10 +122,12 @@
     (if-let [prev (ont-key @ont-metadata)]
       (swap! ont-metadata
              (fn [current]
-               (merge-with conj current {ont-key {:last-request {:request-date (:request-date prev),
-                                                                 :request-url (:request-url prev),
-                                                                 :directory (:directory prev),
-                                                                 :error-log "TO BE IMPLEMENTED"}}}))))))
+               (merge-with conj current {ont-key
+                                         {:last-request
+                                          {:request-date (:request-date prev),
+                                           :request-url (:request-url prev),
+                                           :directory (:directory prev),
+                                           :error-log "TO BE IMPLEMENTED"}}}))))))
 (defn update-metadata
   "Updates ont-metadata with new details."
   [new-details]
@@ -122,6 +139,10 @@
   "Writes the ont-metadata to catalog.edn"
   []
   (pp/pprint @ont-metadata (io/writer +catalog+)))
+
+;; Store requests in vector instead (log of operations in order done)
+;; Don't use 'ontology key'
+;; Can go through and see which are the requests for the same ontology IRI
 
 (defn -main
   "Downloads the ontology into given dir and updates the catalog.edn the metadata."
