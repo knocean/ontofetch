ff9e48e5b85db64df24aaacc179cecfe60e7d994
Rebecca Tauber <becky.tauber@ninepts.com>
2017-11-13 21:37:15 -0800

    Support ftp

--- src/ontofetch/http.clj
+++ src/ontofetch/http.clj
@@ -4,21 +4,22 @@
    [ontofetch.parse.xml :as xml]
    [org.httpkit.client :as http]))
 
-;; TODO: Handle ftp
 (defn get-redirects
   "Manually follows redirects and returns a vector containing all redirect URLs.
    The final URL with content is the last entry."
   [url]
   (loop [redirs []
          new-url url]
-    (let [{:keys [status headers body error]
-           :as res} @(http/request {:url new-url
-                                    :method :head
-                                    :follow-redirects false})]
-      (case status
-        200 (conj redirs new-url)
-        (301 302 303 307 308) (recur (conj redirs new-url) (:location headers))
-        nil))))       ;; Anthing else will not be returned
+    (if (.contains new-url "ftp://")
+      (conj redirs new-url)
+      (let [{:keys [status headers body error]
+             :as res} @(http/request {:url new-url
+                                      :method :head
+                                      :follow-redirects false})]
+        (case status
+          200 (conj redirs new-url)
+          (301 302 303 307 308) (recur (conj redirs new-url) (:location headers))
+          nil)))))       ;; Anthing else will not be returned
 
 (defn invoke-timeout [f timeout-ms]
   (let [thr (Thread/currentThread)
@@ -51,14 +52,20 @@
       (let [purl (first purls)]
         (if-let [final-url (last (get-redirects purl))]
           (do
-            (fetch-ontology! (u/get-path-from-purl dir purl) final-url)
+            (fetch-ontology!
+             (u/get-path-from-purl dir purl)
+             final-url)
             (if-not (empty? (rest purls))
-              (recur (rest purls) (conj fetched purl))
+              (recur
+               (rest purls)
+               (conj fetched purl))
               (conj fetched purl)))
           (do
             (println "Cannot fetch direct import " purl)
             (if-not (empty? (rest purls))
-              (recur (rest purls) fetched)
+              (recur
+               (rest purls)
+               fetched)
               fetched)))))))
 
 (defn fetch-indirect!
@@ -66,18 +73,28 @@
    map of direct imports (key) and their indirect imports (val)."
   [dir imports]
   (if-not (nil? imports)
-    (loop [purls imports i-map {} fetched #{}]
+    (loop [purls imports
+           i-map {}
+           fetched #{}]
       (let [parent (first purls)
             others (rest purls)
-            more-imports (xml/get-imports (xml/get-metadata-node (u/get-path-from-purl dir parent)))]
+            more-imports (->> parent
+                              (u/get-path-from-purl dir)
+                              xml/get-metadata-node
+                              xml/get-imports)]
         (doseq [purl more-imports]
           (if-let [final-url (last (get-redirects purl))]
             (if-not (contains? fetched purl)
-              (fetch-ontology! (u/get-path-from-purl dir purl) final-url))
+              (fetch-ontology!
+               (u/get-path-from-purl dir purl) final-url))
             (println "Cannot fetch indirect import " purl)))
         (if-not (empty? others)
-          (recur others (into i-map {parent more-imports}) (into fetched more-imports))
-          (into i-map {parent more-imports}))))))
+          (recur
+           others
+           (into i-map {parent more-imports})
+           (into fetched more-imports))
+          (into i-map
+                {parent more-imports}))))))
 
 (defn map-imports
   "Given a list of direct imports and a directory,
