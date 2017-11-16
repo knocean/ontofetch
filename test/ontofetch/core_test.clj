79d2b9002553ff4c31706e1d9d68a486c5d4bc33
Rebecca Tauber <becky.tauber@ninepts.com>
2017-11-16 12:10:05 -0500

    Add test ontologies

--- test/ontofetch/core_test.clj
+++ test/ontofetch/core_test.clj
@@ -1,78 +1,67 @@
 (ns ontofetch.core-test
   (:require 
     [clojure.test :refer :all]
-    [ontofetch.http :as http]
     [ontofetch.parse.jena :as jena]
     [ontofetch.parse.owl :as owl]
     [ontofetch.parse.xml :as xml]
     [ontofetch.utils :as u]
-    [clojure.java.io :as io]
-    [clojure.tools.file-utils :as ctfu]))
+    [clojure.java.io :as io]))
 
-(def dir "test/resources/temp")
+(def owl-dir "test/resources/owl")
+(def jena-dir "test/resources/jena")
+(def xml-dir "test/resources/xml")
+(def file "/test-1.owl")
+(def file-ttl "/test-1.ttl")
 
-;; TODO: Build cleaner test ontology,
-;;       add local import files we don't have to fetch each time
-
-(def md {:ontology-iri "http://purl.obolibrary.org/obo/ro.owl",
-         :version-iri
-         "http://purl.obolibrary.org/obo/ro/releases/2017-10-02/ro.owl",
+(def md {:ontology-iri "http://test.com/resources/test-1.owl",
+         :version-iri "http://test.com/resources/2017/test-1.owl",
          :imports
-         {"http://purl.obolibrary.org/obo/ro/rohom.owl" [],
-          "http://purl.obolibrary.org/obo/ro/core.owl"
-          ["http://purl.obolibrary.org/obo/ro/annotations.owl"
-           "http://purl.obolibrary.org/obo/ro/bfo-axioms.owl"
-           "http://purl.obolibrary.org/obo/ro/bfo-classes-minimal.owl"],
-          "http://purl.obolibrary.org/obo/ro/temporal-intervals.owl"
-          ["http://purl.obolibrary.org/obo/ro/annotations.owl"
-           "http://purl.obolibrary.org/obo/ro/core.owl"],
-          "http://purl.obolibrary.org/obo/ro/go-biotic.owl" [],
-          "http://purl.obolibrary.org/obo/ro/pato_import.owl" [],
-          "http://purl.obolibrary.org/obo/ro/go_cc_import.owl" [],
-          "http://purl.obolibrary.org/obo/ro/go_mf_import.owl" [],
-          "http://purl.obolibrary.org/obo/ro/annotations.owl" [],
-          "http://purl.obolibrary.org/obo/ro/el-constraints.owl" []}})
+         {"http://test.com/resources/test-2.owl"
+          ["http://test.com/resources/test-4.owl"],
+          "http://test.com/resources/test-3.owl" []}})
 
-(def test-jena "test/resources/ro.ttl")
+(def md-ttl {:ontology-iri "http://test.com/resources/test-1.ttl",
+             :version-iri "http://test.com/resources/2017/test-1.ttl",
+             :imports
+             {"http://test.com/resources/test-2.ttl"
+              ["http://test.com/resources/test-4.ttl"],
+              "http://test.com/resources/test-3.ttl" []}})
+
+;; ----------------------------------------------------------
+;;                     INTEGRATION TEST
+;; ----------------------------------------------------------
+
+(def test-jena (str jena-dir file-ttl))
 (def md-from-jena
   (let [trps (jena/read-triples test-jena)]
-    (.mkdir (java.io.File. dir))
-    (let [imports (jena/get-imports trps)]
-      ((partial http/fetch-imports! dir) imports)
-      (let [md (u/map-metadata
-                [(jena/get-ontology-iri trps)
-                 (jena/get-version-iri trps)
-                 (jena/get-more-imports imports dir)])]
-        (ctfu/recursive-delete (io/file dir))
-        md))))
+    (u/map-metadata
+     [(jena/get-ontology-iri trps)
+      (jena/get-version-iri trps)
+      (jena/get-more-imports
+       (jena/get-imports trps)
+       jena-dir)])))
 
-(def test-owlapi "test/resources/ro.omn")
-(def md-from-owlapi
-  (let [ont (owl/load-ontology test-owlapi)]
-    (.mkdir (java.io.File. dir))
-    (let [imports (owl/get-imports ont)]
-      ((partial http/fetch-imports! dir) imports)
-      (let [md (u/map-metadata
-                [(owl/get-ontology-iri ont)
-                 (owl/get-version-iri ont)
-                 (owl/get-more-imports imports dir)])]
-        (ctfu/recursive-delete (io/file dir))
-        md))))
+(def test-owl (str owl-dir file))
+(def md-from-owl
+  (let [ont (owl/load-ontology test-owl)]
+    (u/map-metadata
+     [(owl/get-ontology-iri ont)
+      (owl/get-version-iri ont)
+      (owl/get-more-imports
+       (owl/get-imports ont)
+       owl-dir)])))
 
-(def test-xml "test/resources/ro.owl")
+(def test-xml (str xml-dir file))
 (def md-from-xml
   (let [xml (xml/get-metadata-node test-xml)]
-    (.mkdir (java.io.File. dir))
-    (let [imports (xml/get-imports xml)]
-      ((partial http/fetch-imports! dir) imports)
-      (let [md (u/map-metadata
-                [(xml/get-ontology-iri xml)
-                 (xml/get-version-iri xml)
-                 (xml/get-more-imports imports dir)])]
-        (ctfu/recursive-delete (io/file dir))
-        md))))
+    (u/map-metadata
+     [(xml/get-ontology-iri xml)
+      (xml/get-version-iri xml)
+      (xml/get-more-imports
+       (xml/get-imports xml)
+       xml-dir)])))
 
 (deftest get-metadata
-  (is (= md md-from-jena))
-  (is (= md md-from-owlapi))
+  (is (= md-ttl md-from-jena))
+  (is (= md md-from-owl))
   (is (= md md-from-xml)))
